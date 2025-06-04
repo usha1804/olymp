e2c40fb3f16a1e8e08d2f90ac44e8639ea6c1670
Gyandeep7 <142395338+Gyandeep7@users.noreply.github.com>
2025-06-04 12:54:48 +0530

    delete backend

--- backend/model/School.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schoolSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  location: String,
-  established: Number
-});
-
-module.exports = mongoose.model('School', schoolSchema);
